38fc4bb1df904317bfbb0b99319250e49a9d754e
wilches018 <106088959+wilches018@users.noreply.github.com>
2023-10-18 11:56:31 -0500

    Update tabla.js

--- formulary/tabla.js
+++ formulary/tabla.js
@@ -1,23 +1,326 @@
-var row="";
-var cars = [];
-function addrow() {
 
-    const correo = document.getElementById('correo');
-    const descripcion = document.getElementById('descripcion');
-    const addr = document.getElementById('add');
-    row += `<tr>
-        <th scope="row">${correo.value}</th>
-        <td>${descripcion.value}</td>
-    </tr>`;
-    addr.innerHTML = row;
+// funcion de guardar
+function guardar_herramientas() {
+  // const id = document.getElementById("id"); 
+  const nombre = document.getElementById("nombre").value;
+  const Bodega = document.getElementById("Bodega").value;
+  const Estante = document.getElementById("Estante").value;
+  const Gaveta = document.getElementById("Gaveta").value;
+  const rubro = document.getElementById("rubro").value;
+  const Fecha = document.getElementById("Fecha").value;
+  const Cantidad = document.getElementById("Cantidad").value;
+  const Codigo = document.getElementById("Codigo").value;
+  const Estado = document.getElementById("Estado").value;
+  const Descripcion = document.getElementById("Descripcion").value;
 
-}
-function agregarfrt(){
-const addr = document.getElementById('agregarfrt');
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+
+  axios.post('/fronted/guardar_herramientas', {
+    nombre: nombre,
+    Bodega: Bodega,
+    Estante: Estante,
+    Gaveta: Gaveta,
+    rubro: rubro,
+    Fecha: Fecha,
+    Cantidad: Cantidad,
+    Codigo: Codigo,
+    Estado: Estado,
+    Descripcion: Descripcion
+
+
+
+  }, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  }
+  )
+    .then((res) => {
+      console.log(res.data)
+      if (res.data === 'la herramienta ya existe') {
+        // Mostrar la alerta indicando que el correo ya está registrado
+        Swal.fire({
+          icon: 'error',
+          title: 'error',
+          text: 'esta herraminenta ya esta guardada!',
+        })
+      
+        // Restablecer los campos de entrada
+        document.getElementById("nombre").value = "";
+        document.getElementById("Bodega").value = "";
+        document.getElementById("Estante").value = "";
+        document.getElementById("Gaveta").value = "";
+        document.getElementById("rubro").value = "";
+        document.getElementById("Fecha").value = "";
+        document.getElementById("Cantidad").value = "";
+        document.getElementById("Codigo").value = "";
+        document.getElementById("Estado").value = "";
+        document.getElementById("Descripcion").value = "";
+
+      } else {
+        mostrar();
+        // Mostrar la alerta de éxito
+        Swal.fire({
+          position: 'top-center',
+          icon: 'success',
+          title: '¡Registrado Exitosamente!',
+          showConfirmButton: false,
+          timer: 2000,
+  
+
+        });
+
+        document.getElementById("nombre").value = "";
+        document.getElementById("Bodega").value = "";
+        document.getElementById("Estante").value = "";
+        document.getElementById("Gaveta").value = "";
+        document.getElementById("rubro").value = "";
+        document.getElementById("Fecha").value = "";
+        document.getElementById("Cantidad").value = "";
+        document.getElementById("Codigo").value = "";
+        document.getElementById("Estado").value = "";
+        document.getElementById("Descripcion").value = "";
+      }
+
+    })
+    .catch((error) => {
+      console.error(error);
+
+
+    });
 }
-console.log(fila);
+// fin de funcion de guardar
+
+// mostrar datos de la db a la tabla html
+let divcate = document.getElementById('tabla');
+function mostrar() {
+  axios.get('mostrars', {
+    responseType: 'json'
+  })
+    .then(function (response) {
+      let datos = response.data
+      var length = (Object.keys(datos).length) + 1;
+      let mostrar = '';
+     
+      for (let index = 1; index < length; index++) {
+        mostrar += ` <tr>   
+                <td >${datos[index].id} </td>
+                <td>${datos[index].nombre}  </td>
+                <td>${datos[index].bodega}  </td> 
+                <td>${datos[index].estante}  </td> 
+                <td>${datos[index].gaveta}  </td> 
+                <td>${datos[index].rubro}  </td> 
+                <td>${datos[index].fecha}  </td>
+                <td>${datos[index].cantidad}  </td> 
+                <td>${datos[index].codigo}  </td> 
+                <td>${datos[index].estado}  </td> 
+                <td>${datos[index].descripcion}  </td> 
+                <td>
+                <button class="chanfle" onclick="castisgo(${datos[index].id}, '${datos[index].nombre}', '${datos[index].bodega}',
+                '${datos[index].estante}', '${datos[index].gaveta}', '${datos[index].rubro}', '${datos[index].fecha}', '${datos[index].cantidad}', 
+                '${datos[index].codigo}', '${datos[index].estado}', '${datos[index].descripcion}')"> <i class="fas fa-sync"></i></button>
+                <button class="chanfle2" onclick="eliminar(${datos[index].id})"> <i class="fas fa-trash"></i></button>
+              </tr> `;
+
+      }
+      divcate.innerHTML = mostrar
+    })
+    .catch(function (error) {
+      // Maneja los errores aquí
+      console.log(error);
+    });
 }
+window.addEventListener('load', function () {
+  mostrar();
+})
+// FIN DEL MOSTRA
+
+// funcion que abre el modal para actualizar
+function castisgo(id , nombre , bodega , estante , gaveta , rubro , fecha , cantidad , codigo , estado , descripcion ) {
+  var modal7 = document.getElementById("modal7");
+
+  const nuevo_id = document.getElementById('id-e')
+  nuevo_id.value = id
+
+  const nuevo_nombre = document.getElementById('nombre2')
+  nuevo_nombre.value = nombre
+
+  const nuevo_bodega = document.getElementById('Bodega2')
+  nuevo_bodega.value = bodega
+
+  const nuevo_estante = document.getElementById('Estante2')
+  nuevo_estante.value = estante
+
+  const nuevo_gaveta = document.getElementById('Gaveta2')
+  nuevo_gaveta.value = gaveta
+
+  const nuevo_rubro = document.getElementById('rubro2')
+  nuevo_rubro.value = rubro
+
+
+  const nuevo_fecha = document.getElementById('Fecha2')
+  nuevo_fecha.value = fecha
+
+  console.log(fecha)
+
+
+  const nuevo_cantidad = document.getElementById('Cantidad2')
+  nuevo_cantidad.value = cantidad
+
+
+  const nuevo_codigo = document.getElementById('Codigo2')
+  nuevo_codigo.value = codigo
+
+
+  const nuevo_estado = document.getElementById('Estado2')
+  nuevo_estado.value = estado
+
+  const nuevo_descripcion = document.getElementById('Descripcion2')
+  nuevo_descripcion.value = descripcion
+
+
+
+
+  modal7.style.display = "block";
+
+  modal7.addEventListener('click', function (event) {
+  if (event.target === modal7) {
+    modal7.style.display = 'none';
+  }
+  mostrar();
+});
+}
+// fin de abrir el modal para actualizar
+
+// funcion de actualizar
+function update() {
+  const ontide = document.getElementById('id-e')
+  const nombre = document.getElementById('nombre2');
+  const Bodega = document.getElementById('Bodega2')
+  const Estante = document.getElementById('Estante2');
+  const Gaveta = document.getElementById('Gaveta2');
+  const rubro = document.getElementById('rubro2');
+  const fecha = document.getElementById('Fecha2').value;
+  const Cantidad = document.getElementById('Cantidad2');
+  const Codigo = document.getElementById('Codigo2');
+  const Estado = document.getElementById('Estado2');
+  const Descripcion = document.getElementById('Descripcion2');
+
+  axios
+    .post(
+      'actualizar',
+      {
+        id: ontide.value,
+        nombre: nombre.value,
+        Bodega: Bodega.value,
+        Estante: Estante.value,
+        Gaveta: Gaveta.value,
+        rubro: rubro.value,
+        fecha: fecha,
+        Cantidad: Cantidad.value,
+        Codigo: Codigo.value,
+        Estado: Estado.value,
+        Descripcion: Descripcion.value
+      },
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+    )
+    .then((res) => {
+      console.log(res.data)
+      if (res.data == 'message')
+      console.log("dato actualizado")
+      Swal.fire({
+        position: 'top-center',
+        icon: 'success',
+        title: 'Herramienta actualizada exitosamente!',
+        showConfirmButton: false,
+        timer: 2000,
+      })
+      
+        
+
+    })
+    .catch((error) => {
+      console.error(error)
+      
+    })
+}
+// fin de la funcion actualizar
+
+// funcion de eliminar los datos de la db
+function eliminar(id) {
+  Swal.fire({
+    title: '¿Desea eliminar esta herramienta?',
+    icon: 'info',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: 'red',
+    confirmButtonText: 'Aceptar'
+  }).then((result) => {
+    if (result.isConfirmed) {
+  axios.post('delete', {
+    ids: id
+  })
+    .then(function (response) {ƒ
+      console.log(response);
+      Swal.fire({
+        position: 'top-center',
+        icon: 'success',
+        title: 'Herramienta Eliminado Exitosamente!',
+        showConfirmButton: false,
+        timer: 2000,
+      })
+      mostrar();
+    })
+    .catch(function (error) {
+      console.log(error);
+      Swal.fire({
+        position: 'top-center',
+        icon: 'error',
+        title: '¡No puedo eliminar verifique su conecion de internet!',
+        showConfirmButton: false,
+        timer: 3000,
+      })
+    });
+  } else {
+    Swal.fire({
+      title: '¡Proceso cancelado!',
+      icon: 'error'
+    })
+  }
+})
+}
+// fin de la funcion de eliminar los datos de la db 
+
+// funcion de buscar datos en l tabla
+function searchTable() {
+  // Obtiene el valor del campo de búsqueda
+  var input = document.getElementById("searchInput").value.toUpperCase();
+
+  // Obtiene la tabla y los registros de la misma
+  var table = document.getElementById("tabla");
+  var rows = table.getElementsByTagName("tr");
+
+  // Itera sobre los registros y los oculta si no coinciden con la búsqueda
+  for (var i = 0; i < rows.length; i++) {
+    var cells = rows[i].getElementsByTagName("td");
+    var match = false;
+    for (var j = 0; j < cells.length; j++) {
+      if (cells[j].innerHTML.toUpperCase().indexOf(input) > -1) {
+        match = true;
+        break;
+      }
+    }
+    if (match) {
+      rows[i].style.display = "";
+    } else {
+      rows[i].style.display = "none";
+    }
+  }
+}
+// fin del buscador
+
+
